944bb1a17ed94466b568d38ba9e47df7845fb2d8
vaibhav21soni <vaibhavsoni5567@gmail.com>
2025-07-11 15:18:42 +0530

    techStack.jsx updated

--- src/data/techStack.jsx
+++ src/data/techStack.jsx
@@ -1,44 +1,47 @@
 import ReactJs, { AWS, Terraform, Jenkins, Git, Kubernetes, Nginx } from "../components/SkilIcons"
 
 
+const iconStyle = {
+  width: "35px",
+  height: "35px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
 const TechStack = [
-    {
-        id: 1,
-        icon: <AWS width={35} height1={20} height2={15} />,
-        name: "AWS",
-
-    },
-    {
-        id: 2,
-        icon: <Terraform width={35} height1={20} height2={15} />,
-        name: "Terraform",
-
-    },
-    {
-        id: 3,
-        icon: <Jenkins width={35} height1={20} height2={15} />,
-        name: "Jenkins",
-
-    },
-    {
-        id: 4,
-        icon: <Kubernetes width={35} height1={20} height2={15} />,
-        name: "Kubernetes",
-
-    },
-    {
-        id: 5,
-        icon: <Git width={35} height1={20} height2={15} />,
-        name: "Git",
-
-    },
-    {
-        id: 6,
-        icon: <Nginx width={35} height1={20} height2={15} />,
-        name: "Nginx",
-
-    },
+  {
+    id: 1,
+    icon: <div style={iconStyle}><AWS /></div>,
+    name: "AWS",
+  },
+  {
+    id: 2,
+    icon: <div style={iconStyle}><Terraform /></div>,
+    name: "Terraform",
+  },
+  {
+    id: 3,
+    icon: <div style={iconStyle}><Jenkins /></div>,
+    name: "Jenkins",
+  },
+  {
+    id: 4,
+    icon: <div style={iconStyle}><Kubernetes /></div>,
+    name: "Kubernetes",
+  },
+  {
+    id: 5,
+    icon: <div style={iconStyle}><Git /></div>,
+    name: "Git",
+  },
+  {
+    id: 6,
+    icon: <div style={iconStyle}><Nginx /></div>,
+    name: "Nginx",
+  },
 ]
+
 
 export default TechStack;
 
