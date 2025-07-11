87ce162bdfdf39390ac82b1c836278b33c4a3bc8
vaibhav21soni <vaibhavsoni5567@gmail.com>
2025-07-11 15:06:56 +0530

    techStack.jsx updated

--- src/data/techStack.jsx
+++ src/data/techStack.jsx
@@ -4,19 +4,19 @@ import ReactJs, { AWS, Terraform, Jenkins, Git, Kubernetes, Nginx } from "../com
 const TechStack = [
     {
         id: 1,
-        icon: <AWS width={25} height={30} />,
+        icon: <AWS width={35} height1={20} height2={15} />,
         name: "AWS",
 
     },
     {
         id: 2,
-        icon: <Terraform width={22} height={30} />,
+        icon: <Terraform width={35} height1={20} height2={15} />,
         name: "Terraform",
 
     },
     {
         id: 3,
-        icon: <Jenkins width={25} height={30} />,
+        icon: <Jenkins width={35} height1={20} height2={15} />,
         name: "Jenkins",
 
     },
@@ -28,13 +28,13 @@ const TechStack = [
     },
     {
         id: 5,
-        icon: <Git width={27} height={30} />,
+        icon: <Git width={35} height1={20} height2={15} />,
         name: "Git",
 
     },
     {
         id: 6,
-        icon: <Nginx width={25} height={30} />,
+        icon: <Nginx width={35} height1={20} height2={15} />,
         name: "Nginx",
 
     },
