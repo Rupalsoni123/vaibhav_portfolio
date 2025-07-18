cc6c3a5c4202227ae8097e8028bb85abab9a909e
vaibhav21soni <vaibhavsoni5567@gmail.com>
2025-07-18 18:13:54 +0530

    sort  /data/skills.jsx

--- src/data/skills.jsx
+++ src/data/skills.jsx
@@ -3,6 +3,273 @@ import ReactJs, { Bash, Bootstrap, C, Canva, CPlusPlus, CSS, Docker, ExpressJS, 
 
 // 
 const skills = [
+    {
+        id: 35,
+        icon: <AWS />,
+        name: "AWS",
+        style: {
+            shadow: "shadow-[#FF9902]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 36,
+        icon: <AzureIcon />,
+        name: "Azure",
+        style: {
+            shadow: "shadow-[#3481d3]",
+            cover: "from-black to-[#3481d3] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+
+
+
+    {
+        id: 49,
+        icon: <Terraform />,
+        name: "Terraform",
+        style: {
+            shadow: "shadow-[#5C4EE5]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 50,
+        icon: <TerragruntLogo />,
+        name: "TerraGrunt",
+        style: {
+            shadow: "shadow-[#5C4EE5]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+
+    {
+        id: 31,
+        icon: <Docker />,
+        name: "Docker",
+        style: {
+            shadow: "shadow-[#0091E2]",
+            cover: "from-black to-[#0091E2] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 32,
+        icon: <Kubernetes />,
+        name: "Kubernetes",
+        style: {
+            shadow: "shadow-[#326DE6]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 16,
+        icon: <Git />,
+        name: "Git",
+        style: {
+            shadow: "shadow-[#F34F29]",
+            cover: "from-black to-[#F34F29] rounded-b-md",
+            opacity: "opacity-80"
+        }
+    },
+    {
+        id: 17,
+        icon: <GitHub />,
+        name: "GitHub",
+        style: {
+            shadow: "shadow-[#5c5a5a]",
+            cover: "from-black to-[#5c5a5a] rounded-b-md",
+            opacity: "opacity-80"
+        }
+    },
+    {
+        id: 40,
+        icon: <GithubActions />,
+        name: "GH Actions",
+        style: {
+            shadow: "shadow-[#2188FF]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 41,
+        icon: <Gitlab />,
+        name: "Gitlab",
+        style: {
+            shadow: "shadow-[#FCA326]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 51,
+        icon: <Jenkins />,
+        name: "Jenkins",
+        style: {
+            shadow: "shadow-[#5C4EE5]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 52,
+        icon: <Bitbucket />,
+        name: "Bitbucket",
+        style: {
+            shadow: "shadow-[#3481d3]",
+            cover: "from-black to-[#3481d3] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 42,
+        icon: <Grafana />,
+        name: "Grafana",
+        style: {
+            shadow: "shadow-[#F15A2A]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+
+    {
+        id: 43,
+        icon: <Prometheus />,
+        name: "Prometheus",
+        style: {
+            shadow: "shadow-[#F15A2A]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 26,
+        icon: <Bash />,
+        name: "Bash",
+        style: {
+            shadow: "shadow-[#4dbc85]",
+            cover: "from-black to-[#4dbc85] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 46,
+        icon: <Nginx />,
+        name: "Nginx",
+        style: {
+            shadow: "shadow-[#039900]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 37,
+        icon: <Ansible />,
+        name: "Ansible",
+        style: {
+            shadow: "shadow-[#FFFFFF]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 47,
+        icon: <RabbitMQ />,
+        name: "RabbitMQ",
+        style: {
+            shadow: "shadow-[#FF6602]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 48,
+        icon: <SonarQube />,
+        name: "SonarQube",
+        style: {
+            shadow: "shadow-[#549DD0]",
+            cover: "from-black to-[#326DE6] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+
+    {
+        id: 4,
+        icon: <TS />,
+        name: "TS",
+        style: {
+            shadow: "shadow-[#3481d3]",
+            cover: "from-black to-[#3481d3] rounded-b-md",
+            opacity: "opacity-80"
+        }
+    },
+    {
+        id: 5,
+        icon: <MongoDB />,
+        name: "MongoDB",
+        style: {
+            shadow: "shadow-[#4FAA41]",
+            cover: "from-black to-[#4FAA41] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 13,
+        icon: <MySQL />,
+        name: "MySQL",
+        style: {
+            shadow: "shadow-[#00618A]",
+            cover: "from-black to-[#00618A] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 14,
+        icon: <PostGreSql />,
+        name: "PostgreSql",
+        style: {
+            shadow: "shadow-[#336791]",
+            cover: "from-black to-[#336791] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 27,
+        icon: <Redis />,
+        name: "Redis",
+        style: {
+            shadow: "shadow-[#C6302B]",
+            cover: "from-black to-[#C6302B] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
     // {
     //     id: 1,
     //     icon: <HTML />,
@@ -33,27 +300,7 @@ const skills = [
     //         opacity: "opacity-80"
     //     }
     // },
-    {
-        id: 4,
-        icon: <TS />,
-        name: "TS",
-        style: {
-            shadow: "shadow-[#3481d3]",
-            cover: "from-black to-[#3481d3] rounded-b-md",
-            opacity: "opacity-80"
-        }
-    },
-    {
-        id: 5,
-        icon: <MongoDB />,
-        name: "MongoDB",
-        style: {
-            shadow: "shadow-[#4FAA41]",
-            cover: "from-black to-[#4FAA41] rounded-b-md",
-            opacity: "opacity-80"
 
-        }
-    },
     // {
     //     id: 6,
     //     icon: <ExpressJS />,
@@ -129,39 +376,7 @@ const skills = [
 
     //     }
     // },
-    {
-        id: 13,
-        icon: <MySQL />,
-        name: "MySQL",
-        style: {
-            shadow: "shadow-[#00618A]",
-            cover: "from-black to-[#00618A] rounded-b-md",
-            opacity: "opacity-80"
 
-        }
-    },
-    {
-        id: 14,
-        icon: <PostGreSql />,
-        name: "PostgreSql",
-        style: {
-            shadow: "shadow-[#336791]",
-            cover: "from-black to-[#336791] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
-    {
-        id: 27,
-        icon: <Redis />,
-        name: "Redis",
-        style: {
-            shadow: "shadow-[#C6302B]",
-            cover: "from-black to-[#C6302B] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
     // {
     //     id: 15,
     //     icon: <FireBase />,
@@ -173,48 +388,7 @@ const skills = [
 
     //     }
     // },
-    {
-        id: 16,
-        icon: <Git />,
-        name: "Git",
-        style: {
-            shadow: "shadow-[#F34F29]",
-            cover: "from-black to-[#F34F29] rounded-b-md",
-            opacity: "opacity-80"
-        }
-    },
-    {
-        id: 17,
-        icon: <GitHub />,
-        name: "GitHub",
-        style: {
-            shadow: "shadow-[#5c5a5a]",
-            cover: "from-black to-[#5c5a5a] rounded-b-md",
-            opacity: "opacity-80"
-        }
-    },
-        {
-        id: 40,
-        icon: <GithubActions />,
-        name: "GH Actions",
-        style: {
-            shadow: "shadow-[#2188FF]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
 
-        }
-    },
-    {
-        id: 41,
-        icon: <Gitlab />,
-        name: "Gitlab",
-        style: {
-            shadow: "shadow-[#FCA326]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
     // {
     //     id: 18,
     //     icon: <PHP />,
@@ -303,17 +477,7 @@ const skills = [
 
     //     }
     // },
-    {
-        id: 26,
-        icon: <Bash />,
-        name: "Bash",
-        style: {
-            shadow: "shadow-[#4dbc85]",
-            cover: "from-black to-[#4dbc85] rounded-b-md",
-            opacity: "opacity-80"
 
-        }
-    },
 
     // {
     //     id: 28,
@@ -348,28 +512,7 @@ const skills = [
 
     //     }
     // },
-    {
-        id: 31,
-        icon: <Docker />,
-        name: "Docker",
-        style: {
-            shadow: "shadow-[#0091E2]",
-            cover: "from-black to-[#0091E2] rounded-b-md",
-            opacity: "opacity-80"
 
-        }
-    },
-    {
-        id: 32,
-        icon: <Kubernetes />,
-        name: "Kubernetes",
-        style: {
-            shadow: "shadow-[#326DE6]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
     // {
     //     id: 33,
     //     icon: <Canva />,
@@ -392,39 +535,8 @@ const skills = [
 
     //     }
     // },
-    {
-        id: 35,
-        icon: <AWS />,
-        name: "AWS",
-        style: {
-            shadow: "shadow-[#FF9902]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
 
-        }
-    },
-    {
-        id: 36,
-        icon: <AzureIcon />,
-        name: "Azure",
-        style: {
-            shadow: "shadow-[#3481d3]",
-            cover: "from-black to-[#3481d3] rounded-b-md",
-            opacity: "opacity-80"
 
-        }
-    },
-    {
-        id: 37,
-        icon: <Ansible />,
-        name: "Ansible",
-        style: {
-            shadow: "shadow-[#FFFFFF]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
     // {
     //     id: 38,
     //     icon: <Kafka />,
@@ -448,29 +560,7 @@ const skills = [
     //     }
     // },
 
-    {
-        id: 42,
-        icon: <Grafana />,
-        name: "Grafana",
-        style: {
-            shadow: "shadow-[#F15A2A]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
 
-        }
-    },
-
-    {
-        id: 43,
-        icon: <Prometheus />,
-        name: "Prometheus",
-        style: {
-            shadow: "shadow-[#F15A2A]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
     // {
     //     id: 44,
     //     icon: <Jira />,
@@ -493,83 +583,7 @@ const skills = [
 
     //     }
     // },
-    {
-        id: 46,
-        icon: <Nginx />,
-        name: "Nginx",
-        style: {
-            shadow: "shadow-[#039900]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
 
-        }
-    },
-    {
-        id: 47,
-        icon: <RabbitMQ />,
-        name: "RabbitMQ",
-        style: {
-            shadow: "shadow-[#FF6602]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
-    {
-        id: 48,
-        icon: <SonarQube />,
-        name: "SonarQube",
-        style: {
-            shadow: "shadow-[#549DD0]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
-    {
-        id: 49,
-        icon: <Terraform />,
-        name: "Terraform",
-        style: {
-            shadow: "shadow-[#5C4EE5]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
-    {
-        id: 50,
-        icon: <TerragruntLogo />,
-        name: "TerraGrunt",
-        style: {
-            shadow: "shadow-[#5C4EE5]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
-    {
-        id: 51,
-        icon: <Jenkins />,
-        name: "Jenkins",
-        style: {
-            shadow: "shadow-[#5C4EE5]",
-            cover: "from-black to-[#326DE6] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
-    {
-        id: 52,
-        icon: <Bitbucket />,
-        name: "Bitbucket",
-        style: {
-            shadow: "shadow-[#3481d3]",
-            cover: "from-black to-[#3481d3] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    }
 
 ]
 export default skills;
